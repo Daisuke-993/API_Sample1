6c3f835b1df36703b1e91d9131bffeca9c7dfb41
root <root@localhost.localdomain>
2020-06-22 12:39:54 +0900

    fix/json-request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class Controller extends BaseController
 {
@@ -19,9 +20,13 @@ class Controller extends BaseController
 
     public function weather($location)
     {
-        $base = "https://api.openweathermap.org/data/2.5/weather";
-        $api_key = "e753db02de38e5d06f1fdb54a251f5d1";
-        $url = $base . "?q=$location&units=metric&appid=" . $api_key;
+        $base = "https://api.openweathermap.org/data/2.5/weather?";
+        $query = array (
+            'q' => $location,
+            'units' => 'metric',
+            'appid' => 'e753db02de38e5d06f1fdb54a251f5d1'
+        );
+        $url = $base . Arr::query($query);
         $weather = json_decode(file_get_contents($url), true);
         $weather_json = json_encode(array(
             'weather' => $weather['weather'][0]['main'],
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,12 +1,13 @@
 @extends('app')
 
 @section('content')
+    
     <div class="col-3">
         <h1 class="mt-4">Where?</h1>
-        <a href="{{ route('weather', $location="Tokyo") }}">Tokyo<br></a>
-        <a href="{{ route('weather', $location="Osaka") }}">Osaka<br></a>
-        <a href="{{ route('weather', $location="Nagoya") }}">Nagoya<br></a>
-        <a href="{{ route('weather', $location="Yokohama") }}">Yokohama<br></a>
+        <a href="{{ route('weather', $location = 'Tokyo') }}">Tokyo<br></a>
+        <a href="{{ route('weather', $location = 'Osaka') }}">Osaka<br></a>
+        <a href="{{ route('weather', $location = 'Nagoya') }}">Nagoya<br></a>
+        <a href="{{ route('weather', $location = 'Yokohama') }}">Yokohama<br></a>
         <form action="{{ route('request') }}" method="POST">
             <div class="form-group">
                 @csrf
